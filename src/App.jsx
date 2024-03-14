8274a2ec716c9297cd37b07bfa55db0ba8321e2b
Debasis Mishra <debasismishra000123@gmail.com>
2024-03-14 23:58:20 +0530

    Start

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Footer from './Components/Footer'
+import Header from './Components/Header'
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App
