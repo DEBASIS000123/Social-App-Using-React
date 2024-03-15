f3c56195d840ec4fb6d37504f860b83cccb59377
Debasis Mishra <debasismishra000123@gmail.com>
2024-03-16 00:04:20 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,22 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Footer from './Components/Footer'
 import Header from './Components/Header'
 import SideBar from './Components/SideBar'
+import Form from './Components/Form'
+import MakeCard from './Components/MakeCard'
+import { useState } from 'react'
+
 
 function App() {
+  const [selectedtab,setselectedtab]=useState("Home");
+
 
   return (
     <div className='Main-div'>
-      <SideBar></SideBar>
+      <SideBar selectedtab={selectedtab}></SideBar>
       <div className="bodyContent">
       <Header></Header>
+        {selectedtab==="Create Post" && <Form></Form>}
+        {selectedtab==="Home" && <MakeCard></MakeCard>}
       <Footer></Footer>
       </div>
     </div>
