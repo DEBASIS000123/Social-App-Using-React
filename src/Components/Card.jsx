20453ab17c6647be682bf73b9fb706422bd438e8
Debasis Mishra <debasismishra000123@gmail.com>
2024-03-18 02:58:54 +0530

    Done

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,10 +1,23 @@
-const Card=()=>{
-  return <div className="card" style={{width:" 18rem"}}>
-  <img src="..." className="card-img-top" alt="..."/>
+import { MdCancel } from "react-icons/md";
+import { FcLike } from "react-icons/fc";
+import { useContext } from "react";
+import { PostList } from "../Store/post-list-store";
+
+const Card=({post})=>{
+const {deletePost}=useContext(PostList);
+
+  return <div className="card cardstyle" style={{width:"auto"}}>
   <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
+    <h5 className="card-title">{post.title}</h5>
+    <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger deletebtn" onClick={()=>deletePost(post.id)}>
+    <MdCancel />
+    </span>
+    <p className="card-text">{post.body}</p>
+    {post.hashtags.map((hashtag)=>(
+      <span key={hashtag+1} className="badge text-bg-primary hashtag">{hashtag}</span>
+    ))}
+    <br></br>
+    <button type="button" className="btn btn-outline-primary liked"><FcLike /> {post.reactions}</button>
   </div>
 </div>
 }
