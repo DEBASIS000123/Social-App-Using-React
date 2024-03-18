9cf7761fd478b56d640ab70b69f49341c3b6b587
Debasis Mishra <debasismishra000123@gmail.com>
2024-03-19 01:16:51 +0530

    Updated

--- src/Components/MakeCard.jsx
+++ src/Components/MakeCard.jsx
@@ -1,11 +1,18 @@
 import { useContext } from "react";
 import Card from "./Card";
 import {PostList as createdposts} from "../Store/post-list-store";
+import Welcome from "./Welcome";
 
 const MakeCard=()=>{
-  const {postList}=useContext(createdposts);
+  const {postList,addinitialPosts}=useContext(createdposts);
 
+  const Welcomebtnclicked=()=>{
+    fetch("https://dummyjson.com/posts")
+    .then((res)=>res.json())
+    .then((data)=>{addinitialPosts(data.posts)});
+  }
   return <>
+  {postList.length==0 && <Welcome Welcomebtnclicked={Welcomebtnclicked}/>}
   {postList.map((post)=>(
     <Card key={post.id} post={post}></Card>
   ))};
