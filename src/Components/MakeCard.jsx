0dbd13663f4c902d81e7bbc6dce82a933b0cb4cd
Debasis Mishra <debasismishra000123@gmail.com>
2024-03-22 23:31:18 +0530

    Done

--- src/Components/MakeCard.jsx
+++ src/Components/MakeCard.jsx
@@ -1,19 +1,35 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import Card from "./Card";
 import {PostList as createdposts} from "../Store/post-list-store";
 import Welcome from "./Welcome";
+import Spinner from "./Spinner";
 
 const MakeCard=()=>{
   const {postList,addinitialPosts}=useContext(createdposts);
+  const [spinnerval,setspinnerval]=useState(false);
 
-  const Welcomebtnclicked=()=>{
-    fetch("https://dummyjson.com/posts")
+  useEffect(()=>{
+    const controller=new AbortController();
+    const signal=controller.signal;
+
+    setspinnerval(true);
+    fetch("https://dummyjson.com/posts",{signal})
     .then((res)=>res.json())
-    .then((data)=>{addinitialPosts(data.posts)});
-  }
+    .then((data)=>{
+      addinitialPosts(data.posts);
+      setspinnerval(false);
+    });
+    return ()=>{
+      controller.abort();
+    }
+  },[]);
+ 
+    
+  
   return <>
-  {postList.length==0 && <Welcome Welcomebtnclicked={Welcomebtnclicked}/>}
-  {postList.map((post)=>(
+  {spinnerval && <Spinner/>}
+  {!spinnerval && postList.length==0 && <Welcome/>}
+  {!spinnerval && postList.map((post)=>(
     <Card key={post.id} post={post}></Card>
   ))};
   
