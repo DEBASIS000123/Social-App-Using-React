0f3f2109d06995118111563cca58b8f067606fb3
Debasis Mishra <debasismishra000123@gmail.com>
2024-03-22 23:31:50 +0530

    Done

--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -1,7 +1,7 @@
-const Welcome=({Welcomebtnclicked})=>{
+const Welcome=()=>{
   return <>
   <h1 className="Welcome">There are no Posts Here!! Create Your Post.</h1>
-  <div className="welcome-btn"><button className="btn btn-primary " onClick={Welcomebtnclicked}>Get post from server</button></div>
+  
   </>
 }
 export default Welcome
