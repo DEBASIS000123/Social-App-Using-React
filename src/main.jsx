e7cc8ba976f96991079431249a2e1b518446692b
Debasis Mishra <debasismishra000123@gmail.com>
2024-03-26 01:25:50 +0530

    Modified

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './routes/App.jsx'
+import Form from './Components/Form.jsx'
+import { RouterProvider,createBrowserRouter } from 'react-router-dom'
+import MakeCard from './Components/MakeCard.jsx'
+
+const router=createBrowserRouter([
+  {path:"/" , element:<App/>,children:[
+    {path:"/" , element:<MakeCard/>},
+    {path:"/create-post", element:<Form/>} ,
+  ]},
+  
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
