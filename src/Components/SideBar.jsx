e8127c34919667576a2a3cbbe47087ff13d15388
Debasis Mishra <debasismishra000123@gmail.com>
2024-03-26 01:24:21 +0530

    DOne

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,4 +1,6 @@
-const SideBar=({selectedtab,setselectedtab})=>{
+import { Link } from "react-router-dom"
+
+const SideBar=()=>{
   return <>
   <div className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark sidebar" style={{width: "280px"}}>
     <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
@@ -8,16 +10,16 @@ const SideBar=({selectedtab,setselectedtab})=>{
     <hr/>
     <ul className="nav nav-pills flex-column mb-auto">
       <li className="nav-item">
-        <a href="#" className={`nav-link text-white ${selectedtab==="Home" && 'active'}`} aria-current="page" onClick={()=>setselectedtab("Home")}>
+        <Link to="/" className={`nav-link text-white`} aria-current="page" >
           <svg className="bi pe-none me-2" width="16" height="16"><use xlinkHref="#home"></use></svg>
           Home
-        </a>
+        </Link>
       </li>
       <li>
-        <a href="#" className={`nav-link text-white ${selectedtab==="Create Post" && 'active'}`} onClick={()=>setselectedtab("Create Post")}>
+        <Link to="/create-post" className={`nav-link text-white `} >
           <svg className="bi pe-none me-2" width="16" height="16"><use xlinkHref="#speedometer2"></use></svg>
           Create Post
-        </a>
+        </Link>
       </li>
       
     </ul>
